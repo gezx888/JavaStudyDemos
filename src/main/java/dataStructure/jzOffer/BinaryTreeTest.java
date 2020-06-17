b5806a54aa96583a238c9a9d42091cb1bb05c03d
xpgzx <1469622853@qq.com>
2020-06-18 00:16:20 +0800

    剑指offer 面试题 55 算法题演练

--- src/main/java/dataStructure/jzOffer/BinaryTreeTest.java
+++ src/main/java/dataStructure/jzOffer/BinaryTreeTest.java
@@ -1,5 +1,6 @@
 package dataStructure.jzOffer;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -87,6 +88,71 @@ public class BinaryTreeTest {
         if(left==null && right==null) return true;
         if(left==null || right==null || left.val!=right.val) return false;
         return recur(left.left,right.right) && recur(left.right,right.left);
+    }
+
+    /**
+     * case55-1：二叉树的深度
+     * 二种方法：一：递归求法：返回左右子树中最大的深度+1即可
+     *
+     * 二：通过层序遍历（需要申请外部临时存储空间）二叉树的层序遍历一般都是借助队列来辅助实现
+     * 循环遍历当层中的所有节点，每次循环中 每一个节点的左右子树不为空的时候需要将它分别加入容器中，然后赋值
+     * 给队列，每一层遍历完之后，树的深度结果res+1，一直层序循环直到队列为空退出！
+     *
+     * @param root
+     * @return
+     */
+    // 方法一：递归
+    private int maxDepth(TreeNode root){
+        if(root==null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+
+    // 通过借助队列 层序遍历二叉树求解
+    private int maxdepth2(TreeNode root){
+        if(root==null) return 0;
+        LinkedList<TreeNode> queue = new LinkedList<>(),tem;
+        int res = 0;
+        queue.add(root);
+        while (!queue.isEmpty()){
+           tem = new LinkedList<>();
+           for(TreeNode node : queue){
+               if(node.left!=null) tem.add(node.left);
+               if(node.right!=null) tem.add(node.right);
+           }
+           queue = tem;
+           res++;
+        }
+        return res;
+    }
+
+    /**
+     * case55-2：判断是否为平衡二叉树
+     * 思路：方法一（从顶之底）：根据平衡二叉树的定义，当判断得到所有节点都满足条件定义后说明此树为平衡二叉树（借助求二叉树深度函数）
+     *
+     * 方法二：后序遍历+剪枝（从底至顶）：思路是对二叉树做后序遍历，从底至顶返回子树深度，若判定某子树不是平衡树则 “剪枝” ，直接向上返回。
+     * 也是需要构造一个外部函数：返回值 当前子树深度（当其左右节点深度差<=1时，返回其深度；当其左右节点深度差>2时，返回-1,代表此树不是平衡二叉树）
+     *
+     * @return
+     */
+    // 方法一：方法一（从顶之底）递归求解
+    private boolean isBalanced(TreeNode root){
+        if(root==null) return true;
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(maxDepth(root.left) - maxDepth(root.right))<=1;
+    }
+
+    // 方法二：后序遍历+剪枝（从底至顶）
+    private boolean isBalanced2(TreeNode root){
+        return recueMethod(root) != -1;
+    }
+
+    // 方法二的辅助函数(下面的-1是自己定义的，你也可以定义成其他数)
+    private int recueMethod(TreeNode root) {
+        if(root==null) return 0;
+        int left = recueMethod(root.left);
+        if(left==-1) return -1;
+        int right = recueMethod(root.right);
+        if(right==-1) return -1;
+        return Math.abs(left-right) < 2?Math.max(left,right)+1:-1;
     }
 
 }
